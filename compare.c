b382402aaf7f35639a927bcad408ab3661ec61ac
Alice Yeh <alice.s.yeh@gmail.com>
2014-08-03 14:21:17 -0700

    adding queue.c/h for BFS implementation

--- compare.c
+++ compare.c
@@ -8,9 +8,29 @@
 #define UNDEFINED (-1)
 
 
+void findShortestPath(graphT g, int u, int v){
+  
 
-void findShortestPath(int u, int v){
-
+1  procedure BFS(G,v) is
+2      create a queue Q
+3      create a set V
+4      add v to V
+5      enqueue v onto Q
+6      while Q is not empty loop
+7         t ← Q.dequeue()
+8         if t is what we are looking for then
+9            return t
+10        end if
+11        for all edges e in G.adjacentEdges(t) loop
+12           u ← G.adjacentVertex(t,e)
+13           if u is not in V then
+14               add u to V
+15               enqueue u onto Q
+16           end if
+17        end loop
+18     end loop
+19     return none
+20 end BFS
 }
 
 /* average number of nodes in the shortest path between all pairs of nodes */
